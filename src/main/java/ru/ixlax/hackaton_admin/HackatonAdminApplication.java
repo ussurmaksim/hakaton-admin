822a526a66024716d5beec83c6200241e7b14b27
Ilya Samsonov <ixlax@mail.ru>
2025-09-20 00:14:57 -0800

    Move Spring Boot project to root

---
+++ src/main/java/ru/ixlax/hackaton_admin/HackatonAdminApplication.java
@@ -0,0 +1,13 @@
+package ru.ixlax.hackaton_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackatonAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackatonAdminApplication.class, args);
+	}
+
+}
